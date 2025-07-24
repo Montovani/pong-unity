4df980e7a2aa2099e78fe396bba0ae53f8115e04
Igor Montovani <igor.montovani@gmail.com>
2025-07-24 12:27:00 +0200

    Changed the ball movement once hit the top and bottom of the map

--- Pong/Assets/Scripts/BallMovement.cs
+++ Pong/Assets/Scripts/BallMovement.cs
@@ -18,18 +18,23 @@ public class BallMovement : MonoBehaviour
     {
         rd.linearVelocity = direction * speed;
     }
-   void OnCollisionEnter2D(Collision2D collision)
-{
-    if (collision.gameObject.CompareTag("Player"))
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        float paddleY = collision.transform.position.y;
-        float ballY = transform.position.y;
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            float paddleY = collision.transform.position.y; //Aqui pega a posicao y do "player
+            float ballY = transform.position.y; //Aqui pega a posicao y da bola
 
-        float difference = ballY - paddleY;
-        float paddleHeight = collision.collider.bounds.size.y;
-        float normalizedY = difference / (paddleHeight / 2);
+            float difference = ballY - paddleY; //Pegar onde no paddle a bola bateu cima meio ou baixo
+            float paddleHeight = collision.collider.bounds.size.y; //colision tinhamos declarado ja, vai pegar o tamanho e posicao do objeto com o .collider depois pega os limites com o .bounds e no final pega o tamanho total do objeto
+            float normalizedY = difference / (paddleHeight / 2); //Aqui ve se bateu no centro =0 cima +1 ou baixo -1
 
-        direction = new Vector2(-direction.x, normalizedY).normalized;
+            direction = new Vector2(-direction.x, normalizedY).normalized;
+        }
+        if (collision.gameObject.CompareTag("xLimit"))
+        {
+            direction = new Vector2(direction.x,-direction.y);
+        }
+    
     }
-}
 }
